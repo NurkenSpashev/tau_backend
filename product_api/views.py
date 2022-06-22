a6368f9e6f467a979bdde5325cdddf0652a7bfa9
Spashev Nurken <s.nurken92@gmail.com>
2022-06-22 09:37:10 +0600

    paginations

--- product_api/views.py
+++ product_api/views.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets
 
 from .models import Product, Review
 from .serializers import ProductSerializer, ReviewSerializer
+from.paginations import ProductPagination, ReviewPagination
 
 
 class IndexView(TemplateView):
@@ -13,9 +14,11 @@ class IndexView(TemplateView):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductPagination
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    pagination_class = ReviewPagination
 

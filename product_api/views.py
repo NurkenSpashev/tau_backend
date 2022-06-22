5141c6500a7da53a41d407801696eb76f831e44c
Spashev Nurken <s.nurken92@gmail.com>
2022-06-22 09:44:45 +0600

    rm template from api

--- product_api/views.py
+++ product_api/views.py
@@ -1,14 +1,8 @@
-from django.views.generic import TemplateView
-
 from rest_framework import viewsets
 
 from .models import Product, Review
 from .serializers import ProductSerializer, ReviewSerializer
 from.paginations import ProductPagination, ReviewPagination
-
-
-class IndexView(TemplateView):
-    template_name = 'products/index.html'
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -21,4 +15,3 @@ class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     pagination_class = ReviewPagination
-
